6fe0a7578c6435b4cb586b3e5284d3b50dab32a2
Sbrunelli433 <brunes46@gmail.com>
2019-06-07 11:19:42 -0500

    created passing tests and failing tests for my Remove() method including both ints and strings.

--- CustomClassList_Project_Tests/UnitTest1.cs
+++ CustomClassList_Project_Tests/UnitTest1.cs
@@ -175,19 +175,180 @@ namespace CustomClassList_Project_Tests
         {
             //arrange
             CustomList<int> testList = new CustomList<int>();
-            testList.Add(0);
             testList.Add(1);
             testList.Add(2);
-            int expected = 1;
+            testList.Add(3);
+            int expected = testList[2];
             int actual;
 
 
             //act
-            testList.Remove(1);
+            testList.Remove(0);
             actual = testList.Count;
 
             //assert
             Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveFirstItemInList_IndexZeroFailure()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            int expected = testList[1];
+            int actual;
+
+
+            //act
+            testList.Remove(0);
+            actual = testList.Count;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+            //expected:<2>, actual<3>;
+        }
+        [TestMethod]
+        public void Remove_RemoveItemInList_IndexTwo()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            testList.Add(6);
+            int expected = testList[4];
+            int actual;
+
+
+            //act
+            testList.Remove(2);
+            actual = testList[3];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveItemInList_IndexTwoFailure()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            testList.Add(6);
+            int expected = testList[3];
+            int actual;
+
+
+            //act
+            testList.Remove(2);
+            actual = testList[3];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+            //expected: <4> actual: <5>;
+        }
+
+        [TestMethod]
+        public void Remove_RemoveItemInList_IndexFive()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            testList.Add(6);
+            int expected = testList[5];
+            int actual;
+
+
+            //act
+            testList.Remove(5);
+            actual = testList[4];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemoveItemInList_IndexFiveFailure()
+        {
+            //arrange
+            CustomList<int> testList = new CustomList<int>();
+            testList.Add(1);
+            testList.Add(2);
+            testList.Add(3);
+            testList.Add(4);
+            testList.Add(5);
+            testList.Add(6);
+            int expected = testList[5];
+            int actual;
+
+
+            //act
+            testList.Remove(5);
+            actual = testList[5];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+            //expected:<6>, actual:<0>;
+        }
+
+        [TestMethod]
+        public void Remove_RemovePokemonFromList_ByeBulbasaur()
+        {
+            //arrange
+            CustomList<string> testList = new CustomList<string>();
+            string bulbasaur = "Bulbasaur";
+            testList.Add(bulbasaur);
+
+            string ivysaur = "Ivysaur";
+            testList.Add(ivysaur);
+
+            string venusaur = "Venusaur";
+            testList.Add(venusaur);
+
+            string expected = testList[1];
+            string actual;
+
+            //act
+            testList.Remove(bulbasaur);
+            actual = testList[0];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemovePokemonFromList_ByeBulbasaurFailure()
+        {
+            //arrange
+            CustomList<string> testList = new CustomList<string>();
+            string bulbasaur = "Bulbasaur";
+            testList.Add(bulbasaur);
+
+            string ivysaur = "Ivysaur";
+            testList.Add(ivysaur);
+
+            string venusaur = "Venusaur";
+            testList.Add(venusaur);
+
+            string expected = testList[1];
+            string actual;
+
+            //act
+            testList.Remove(bulbasaur);
+            actual = testList[1];
+
+            //assert
+            Assert.AreEqual(expected, actual);
+            //expected:<Ivysaur>, actual:<Venusaur>;
         }
     }
 }
